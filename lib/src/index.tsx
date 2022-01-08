535dfd4e81acb5a71345d92e2c41c44cf7332144
C. Lewis <ctj.lewis@icloud.com>
2022-01-08 09:31:17 -0600

    chore: drop workspaces

--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,7 +1,7 @@
 export const WelcomeMessage = () => {
   return (
     <h1 className="text-center">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      Welcome to <a href="https://nextjs.org">Next.js!</a>
     </h1>
   );
 };
