613820acff41bd2f5ccd917deaddd47adbd0f04e
C. Lewis <ctj.lewis@icloud.com>
2022-01-08 09:02:56 -0600

    chore: configure scripts

--- demo/next.config.js
+++ demo/next.config.js
@@ -9,10 +9,12 @@ module.exports = {
   webpack: ({ ...config }) => {
     /**
      * tszip modules require top-level await because ESM requires top-level
-     * await.
+     * await. `next build` Webpack requires `experiments.layers` to be enabled
+     * with this configuration.
      */
     config.experiments = {
-      topLevelAwait: true
+      topLevelAwait: true,
+      layers: true,
     };
     return {
       ...config,
